93fabcf4cb0d62f5796bbc6e8691c933c2d06202
Krzysiek <krzysztof.makuch@gmail.com>
2023-08-08 14:48:27 +0200

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,27 @@
+#import previously done files
+import os
+
+def interf():
+    '''
+
+    '''
+
+    print('What do you want to do?:\
+          \n 0. HELP \
+          \n 1. RDF change in time \
+          \n 2. MSD & diffusion \
+          \n 3. angle vs axis \
+          \n 4. xyz to volume \
+          \n 5. unavailable: H-bond lifetime - work in progress')
+    action = int(input()[0])
+
+    actions = {0: a0,
+               1: a1,
+               2: a2,
+               3: a3,
+               4: a4,
+               5: a5
+               }
+
+
+
